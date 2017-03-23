29bf479ec417ba1f1ec654b918e56b1dc60c11b2
pashute <pashute@gmail.com>
2017-03-23 05:15:42 +0200

    commiting (hopefully) all changes first time

--- setup/odoomain.py
+++ setup/odoomain.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
 # set server timezone in UTC before time module imported
-__import__('os').environ['TZ'] = 'UTC'
+__import__('os').environ['TZ'] = 'IST' # I changed that. It's Israel time'. Ok
 __import__('pkg_resources').declare_namespace('odoo.addons')
 import odoo
-
+ 
 if __name__ == "__main__":
     odoo.cli.main()
+#. yo
